dbba606b6ab44ed9cd92e30f281515854e8b7285
tharunprasanth517 <tharunprasanth25@gmail.com>
2026-03-12 16:04:20 +0530

    Base application setup

---
+++ src/UseCase1HotelBookingApp.java
@@ -0,0 +1,17 @@
+
+
+    public class UseCase1HotelBookingApp {
+
+        public static void main(String[] args) {
+
+            // Display welcome message
+            System.out.println("=====================================");
+            System.out.println(" Welcome to Book My Stay App ");
+            System.out.println(" Hotel Booking Management System ");
+            System.out.println(" Version: 1.0 ");
+            System.out.println("=====================================");
+
+            System.out.println("Application started successfully!");
+
+        }
+    }
